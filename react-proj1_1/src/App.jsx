d001ce43be6659925b07a3b56fabd6958a6d61bf
omBhavsar007 <bhavsarom007@gmail.com>
2026-01-14 09:02:52 -0600

    Add Many Changes

---
+++ react-proj1_1/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Iron from './Iron'
+
+function App() {
+
+  return (
+    <>
+      <Iron/>
+      <h1>Hello Ji</h1>
+    </>
+  )
+}
+
+export default App
---
+++ react-proj1_1/src/Iron.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+function Iron()
+{
+    return(
+        <h1>Here is the Iron</h1>
+    )
+}
+
+export default Iron
