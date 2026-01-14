169de4efa3f6c235d19b5d8c6ae1835d84bb657d
omBhavsar007 <bhavsarom007@gmail.com>
2026-01-14 11:38:43 -0600

    Update main.jsx

--- react-proj1_1/src/main.jsx
+++ react-proj1_1/src/main.jsx
@@ -1,9 +1,40 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+import Iron from './Iron.jsx'
+
+// Types of creating elements in React 
+
+// Direct Tag injection through object
+
+const anotherElement = (
+  <a href='https://google.com'>Visit Google</a>
+)
+
+// Use React.createElement
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://shaastrasamvad.com', target: '_blank'},
+  'click me to visit google'
+)
+
+// Normal Function 
+
+function MyApp()
+{
+  let a = Math.random();
+  let b = Math.random();
+  let sum = a+b
+  return (
+    <p>Her is the sum of {a} and {b}, that is {sum}</p>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+  <App/>
+  <MyApp/>
+  </>
 )
